2b449dcbc09028af9d0638073e7e6369d399b08b
Rian Alves <rianalves2009.ra@gmail.com>
2018-12-01 10:48:51 -0200

    Correções na USFContract

--- app/src/main/java/dcc196/ufjf/br/maissaude/DAO/UsfContract.java
+++ app/src/main/java/dcc196/ufjf/br/maissaude/DAO/UsfContract.java
@@ -6,8 +6,8 @@ public class UsfContract {
     public final class USF implements BaseColumns {
         public final static String TABLE_NAME = "Usf";
         public final static String COLUMN_NAME_UNIDADE = "nome";
-        public static final String COLUMN_NAME_CEP = "temporada";
-        public static final String COLUMN_NAME_FOTO = "episodio";
+        public static final String COLUMN_NAME_CEP = "cep";
+        public static final String COLUMN_NAME_FOTO = "foto";
         public final static String CREATE_UNIDADE  = "CREATE TABLE "+USF.TABLE_NAME+" ("
                 + USF._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + USF.COLUMN_NAME_UNIDADE+ " TEXT, "
@@ -20,11 +20,11 @@ public class UsfContract {
     public final class Area implements BaseColumns {
         public final static String TABLE_NAME = "Area";
         public final static String COLUMN_NAME_CEP = "cep";
-        public static final String COLUMN_NAME_CEP_USF = "cepUSF";
+        public static final String COLUMN_NAME_ID_USF = "idUSF";
         public final static String CREATE_AREA  = "CREATE TABLE "+Area.TABLE_NAME+" ("
                 + Area._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + Area.COLUMN_NAME_CEP+ " TEXT, "
-                + Area.COLUMN_NAME_CEP_USF+ " TEXT,"
+                + Area.COLUMN_NAME_ID_USF+ " TEXT,"
                 +")";
         public final static String DROP_AREA = "DROP TABLE IF EXISTS "+Area.TABLE_NAME;
     }
