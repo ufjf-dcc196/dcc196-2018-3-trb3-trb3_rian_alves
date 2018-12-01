4b95f908f541a56625a8692da6bec2e834de3ad1
Rian Alves <rianalves2009.ra@gmail.com>
2018-12-01 11:43:19 -0200

    Criação da Classe APIViaACEP Criação da Classe BuscaUnidade

---
+++ app/src/main/java/dcc196/ufjf/br/maissaude/BuscaUnidade.java
@@ -0,0 +1,13 @@
+package dcc196.ufjf.br.maissaude;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BuscaUnidade extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_busca_unidade);
+    }
+}
---
+++ app/src/main/java/dcc196/ufjf/br/maissaude/Servico/APIViaCEP.java
@@ -0,0 +1,4 @@
+package dcc196.ufjf.br.maissaude.Servico;
+
+public class APIViaCEP {
+}
